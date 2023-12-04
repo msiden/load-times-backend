1b908e0d48e8593a56cb43f2db2a390a9f4d0fd9
Martin Sidén <martin.siden@bambora.com>
2023-12-04 13:49:46 +0100

    add database

--- src/lt-backend/db.py
+++ src/lt-backend/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Integer
+from sqlalchemy import String, Integer, Float
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
@@ -7,42 +7,37 @@ from sqlalchemy.orm import Session
 from sqlalchemy import select
 from sqlalchemy.exc import OperationalError
 from marshmallow_sqlalchemy import SQLAlchemySchema, auto_field
-import os
+import uuid
 
 
-ENVIRONMENT = os.getenv("NGDB_ENV")
-IS_PROD = ENVIRONMENT == "prod"
-PASSWORD = os.getenv("NGDB_PASSWORD") if IS_PROD else "abc123"
-URL = os.getenv("NGDB_URL") if IS_PROD else "localhost"
-DB_CONFIG = f"postgresql+psycopg2://postgres:{PASSWORD}@{URL}:5432/scores"
 
-engine = create_engine(DB_CONFIG)
+engine = create_engine("sqlite:///database.db")
 
 
 class Base(DeclarativeBase):
     pass
 
 
-class Scores(Base):
-    __tablename__ = "scores"
+class LoadTimes(Base):
+    __tablename__ = "load_times"
     id: Mapped[str] = mapped_column(primary_key=True)
-    user_name: Mapped[str] = mapped_column(String(30))
-    score: Mapped[int] = mapped_column(Integer)
-    level: Mapped[int] = mapped_column(Integer)
+    url: Mapped[str] = mapped_column(String)
+    search_phrase: Mapped[str] = mapped_column(String)
+    start_time: Mapped[str] = mapped_column(String)
+    response_time: Mapped[str] = mapped_column(Float)
+    occurencies: Mapped[int] = mapped_column(Integer)
 
-    def __repr__(self) -> str:
-        return f"id:{self.id!r}, user_name:{self.user_name!r}, score:{self.score!r}, level:{self.level!r}"
-
-
-class ScoresSchema(SQLAlchemySchema):
+class LoadTimesSchema(SQLAlchemySchema):
     class Meta:
-        model = Scores
+        model = LoadTimes
         load_instance = True
 
     id = auto_field()
-    user_name = auto_field()
-    score = auto_field()
-    level = auto_field()
+    url = auto_field()
+    search_phrase = auto_field()
+    start_time = auto_field()
+    response_time = auto_field()
+    occurencies = auto_field()
 
 
 def create_all():
@@ -52,27 +47,33 @@ def create_all():
         print("DB connection failed")
 
 
-def insert_data(id: str, score: int, level: int, user_name: str) -> int:
+def insert_data(url: str, search_phrase: str, start_time: str, response_time: float, occurencies: int) -> uuid.uuid4:
     with Session(engine) as session:
 
-        new_score = Scores(
+        id = str(uuid.uuid4())
+
+        new_entry = LoadTimes(
             id=id,
-            score=score,
-            level=level,
-            user_name=user_name
+            url=url,
+            search_phrase=search_phrase,
+            start_time=start_time,
+            response_time=response_time,
+            occurencies=occurencies
         )
 
-        session.add(new_score)
+        session.add(new_entry)
         session.commit()
 
+    return id
 
-def get_data(chunk_size: int, level: int) -> dict:
-    scores_schema = ScoresSchema(many=True)
+
+def get_data(chunk_size: int) -> dict:
+    load_times_schema = LoadTimesSchema(many=True)
 
     with Session(engine) as session:
-        statement = select(Scores).where(Scores.level == level).order_by(Scores.score.desc()).limit(chunk_size)
+        statement = select(LoadTimes).order_by(LoadTimes.start_time.desc()).limit(chunk_size)
         result = session.scalars(statement).all()
-        return scores_schema.dump(result)
+        return load_times_schema.dump(result)
 
 
 create_all()
--- src/lt-backend/endpoints.py
+++ src/lt-backend/endpoints.py
@@ -1,5 +1,6 @@
-# import db
+import db
 import requests
+from datetime import datetime
 
 
 def get_load_time(url: str, search_phrase) -> dict:
@@ -7,18 +8,27 @@ def get_load_time(url: str, search_phrase) -> dict:
     if not url.startswith("http"):
         url = "http://" + url
 
+    now = str(datetime.now())
+
     response = requests.get(url)
     response.raise_for_status()
 
     page_content = response.text.lower()
 
-    return {
-        "load_time": response.elapsed.total_seconds(),
-        "search_phrase_occurencies": page_content.count(search_phrase.lower())
+    results = {
+        "url": url,
+        "search_phrase": search_phrase,
+        "response_time": response.elapsed.total_seconds(),
+        "occurencies": page_content.count(search_phrase.lower()),
+        "start_time": now
     }
 
+    db.insert_data(**results)
 
-def get_results() -> list:
-    # return db.get_data(chunk_size, level)
-    return [{"results": 1234}]
+    return results
+
+
+def get_results(chunk_size) -> list:
+    return db.get_data(chunk_size)
+
 
